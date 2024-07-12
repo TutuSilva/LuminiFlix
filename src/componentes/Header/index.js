0c1bbf9ef5b167f513a3fe6835658ffe069afcdb
TutuSilva <160740774+TutuSilva@users.noreply.github.com>
2024-07-12 13:46:56 -0300

    few alterations

--- src/componentes/Header/index.js
+++ src/componentes/Header/index.js
@@ -10,7 +10,7 @@ export default ({black}) => {
         </a>
       </div>
       <div className="header--user">
-        <a href="./Form/index.js">
+        <a href="/">
           <img
             src="https://i.pinimg.com/564x/5b/50/e7/5b50e75d07c726d36f397f6359098f58.jpg"
             alt="adicionar usuario"
